bf7906fb2a87a0bc1dc6b81e31dd1d63a90253ac
Chris Roemmich <croemmich@myriadmobile.com>
2018-07-07 01:07:52 -0500

    Make detect_ip.ps1 use Get-NetIpAddress rather than metadata server

--- scripts/detect_ip.ps1
+++ scripts/detect_ip.ps1
@@ -13,7 +13,4 @@
 #    under the License.
 #
 
-$headers = @{"Metadata" = "true"}
-$r = Invoke-WebRequest -headers $headers "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-04-02&format=text" -UseBasicParsing
-$r.Content
-
+(Get-NetIpAddress -InterfaceIndex (Get-NetIPConfiguration | Foreach IPv4DefaultGateway).ifIndex).IPAddress
